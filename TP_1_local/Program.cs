2752ab2594e22bcbe801465b4eb2a583189cd58a
RedMuri <celovekda@gmail.com>
2023-10-03 10:40:12 +0300

    Add division to zero check

--- TP_1_local/Program.cs
+++ TP_1_local/Program.cs
@@ -16,7 +16,7 @@
 	switch (operation)
 	{
 		case '+':
-			result = num1 + num2;
+			result = num1 + num2
 			break;
 		case '-':
 			result = num1 - num2;
@@ -25,10 +25,13 @@
 			result = num1 * num2;
 			break;
 		case '/':
-			result = num1 / num2;
+			if (num2 != 0)
+				result = num1 / num2;
+			else
+				Console.WriteLine("Ошибка! Деление на ноль");
 			break;
 		default:
-			Console.WriteLine("Оперетор указан неверно. Введите +, -, *, или /.");
+			Console.WriteLine("Оперетор указан неверно. Введите +, -, *, или /");
 			continue;
 	}
 
