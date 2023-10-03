efeab22e3e315ddb3c4b8794f1488238e668ad14
RedMuri <celovekda@gmail.com>
2023-10-03 10:26:38 +0300

    Add simple calc actions

---
+++ TP_1_local/Program.cs
@@ -0,0 +1,37 @@
+﻿while (true)
+{
+	Console.Write("Введите первое число: ");
+	double num1;
+	double.TryParse(Console.ReadLine(), out num1);
+
+	Console.Write("Введите оператор (+, -, *, /): ");
+	char operation = Console.ReadLine().Trim()[0];
+
+	Console.Write("Введите второе число: ");
+	double num2;
+	double.TryParse(Console.ReadLine(), out num2);
+
+	double result = 0;
+
+	switch (operation)
+	{
+		case '+':
+			result = num1 + num2;
+			break;
+		case '-':
+			result = num1 - num2;
+			break;
+		case '*':
+			result = num1 * num2;
+			break;
+		case '/':
+			result = num1 / num2;
+			break;
+		default:
+			Console.WriteLine("Оперетор указан неверно. Введите +, -, *, или /.");
+			continue;
+	}
+
+	Console.WriteLine($"Результат: {result}");
+	Console.WriteLine("------");
+}
